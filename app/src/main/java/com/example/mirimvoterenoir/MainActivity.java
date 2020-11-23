087a60e2806d673083ffc05b8d2479df1ff00a65
young <pieangel11@naver.com>
2020-11-23 20:44:15 +0900

    Complete Vote

---
+++ app/src/main/java/com/example/mirimvoterenoir/MainActivity.java
@@ -0,0 +1,38 @@
+package com.example.mirimvoterenoir;
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    String[] imgNames = {"독서하는 소녀", "꽃장식 모자 소녀", "부채를 든 소녀", "이레느깡 단 베르양", "잠자는 소녀", "테라스의 두 자매", "피아노 레슨", "피아노 앞의 소녀들", "해변에서"};
+    int[] voteCount = new int[imgNames.length];
+    ImageView[] imgVs = new ImageView[imgNames.length];
+    int[] imgVIds = {R.id.img1, R.id.img2, R.id.img3, R.id.img4, R.id.img5, R.id.img6, R.id.img7, R.id.img8, R.id.img9};
+//    int index;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        for (int i=0; i< voteCount.length; i++){
+            voteCount[i] = 0;
+        }
+
+        for (int i=0; i < imgVIds.length; i++){
+            final int index;
+            index = i;
+            imgVs[index] = findViewById(imgVIds[index]);
+            imgVs[index].setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    voteCount[index]++;
+                    Toast.makeText(getApplicationContext(), imgNames[index] + ": 총 " + voteCount[index] + "표", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+    }
+}
