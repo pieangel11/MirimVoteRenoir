b3af885fffdb485590d004439e207908f34d86d1
young <pieangel11@naver.com>
2020-11-24 19:44:10 +0900

    Complete ResultActivity

--- app/src/main/java/com/example/mirimvoterenoir/MainActivity.java
+++ app/src/main/java/com/example/mirimvoterenoir/MainActivity.java
@@ -1,8 +1,10 @@
 package com.example.mirimvoterenoir;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Toast;
 
@@ -17,6 +19,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        setTitle("르노와르의 선호 작품 투표");
 
         for (int i=0; i< voteCount.length; i++){
             voteCount[i] = 0;
@@ -34,5 +37,20 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
         }
+
+        Button btnDone = findViewById(R.id.btn_done);
+        btnDone.setOnClickListener(btnListener);
     }
+
+    View.OnClickListener btnListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            Intent intent = new Intent(getApplicationContext(), ResultActivity.class);
+            intent.putExtra("voteCount", voteCount);
+            intent.putExtra("imgNames", imgNames);
+
+            startActivity(intent);
+        }
+    };
+
 }
